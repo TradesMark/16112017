5279c2ab91513feca7149dd34190d2027128e4a5
Anton Galkin <artemis9898@mail.ru>
2017-11-16 12:59:00 +0300

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,1 +1,5 @@
-https://kodaktor.ru/web16112017_c8696
+
+const f = function(t, e){e.target.textContent = this+t};   
+document.querySelector('button:first-child').addEventListener('click', f.bind('Нажата ', 'первая кнопка'));  
+document.querySelector('button:last-child').addEventListener('click', f.bind('Нажата ', 'вторая кнопка'));  
+document.querySelector('h1').insertAdjacentHTML('afterend', '<h4 id="author" title="GossJS">Anton  Galkin</h4>');
