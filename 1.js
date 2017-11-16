cc0439382d133b8c6d1c6e5117bdfd36be1c17d0
Anton Galkin <artemis9898@mail.ru>
2017-11-16 12:59:52 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,1 +1,5 @@
-https://kodaktor.ru/web16112017
+
+const f = function(t, e){e.target.textContent = this+t}; 
+document.querySelector('button').addEventListener('click', f('Нажата первая кнопка'));  
+document.querySelector('button').addEventListener('click', f('Нажата вторая кнопка'));  
+document.querySelector('h1').insertAdjacentHTML('afterend', '<h4 id="author" title="GossJS">Alan Turing</h4>');
